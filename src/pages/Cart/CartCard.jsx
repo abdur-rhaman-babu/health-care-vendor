1c4c6035ac9c223d3c8f07992cf96f4c82828518
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-01-20 14:56:22 +0600

    delete cart from cart

--- src/pages/Cart/CartCard.jsx
+++ src/pages/Cart/CartCard.jsx
@@ -1,5 +1,19 @@
+import { toast } from "react-toastify";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCarts from "../../hooks/useCarts";
+
 const CartCard = ({ item }) => {
-  const { item_name, company, price, quantity } = item;
+  const [, refetch] = useCarts();
+  const { item_name, company, price, quantity, _id } = item;
+  const axiosSecure = useAxiosSecure();
+  const handleDeleteFromCart = async (id) => {
+    const res = await axiosSecure.delete(`/cart/${id}`);
+    console.log(res.data);
+    if(res.data.deletedCount > 0){
+        toast.success(`${item_name} is successfully deleted`)
+        refetch()
+    }
+  };
   return (
     <div className="max-w-sm p-4 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
       <div className="mb-4">
@@ -17,15 +31,14 @@ const CartCard = ({ item }) => {
             -
           </button>
           <span>{quantity}</span>
-          <button
-            className="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300"
-          >
+          <button className="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">
             +
           </button>
         </div>
       </div>
       <div className="mt-4">
         <button
+          onClick={() => handleDeleteFromCart(_id)}
           className="w-full px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
         >
           Delete
